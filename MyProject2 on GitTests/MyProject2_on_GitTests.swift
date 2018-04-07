af35400a6bebe97e96efda816ad17a3b1544f24b
skgc45 <s.sakachan@hotmail.co.jp>
2018-04-07 15:22:27 +0900

    Initial Commit

---
+++ MyProject2 on GitTests/MyProject2_on_GitTests.swift
@@ -0,0 +1,36 @@
+//
+//  MyProject2_on_GitTests.swift
+//  MyProject2 on GitTests
+//
+//  Created by 坂口慎吾 on 2018/04/07.
+//  Copyright © 2018年 坂口慎吾. All rights reserved.
+//
+
+import XCTest
+@testable import MyProject2_on_Git
+
+class MyProject2_on_GitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
